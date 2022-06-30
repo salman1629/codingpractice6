c27d1b442e09f1b76ee28dd70fa6a4fc69a54814
salman <syamsalman0@gmail.com>
2022-06-30 01:25:29 +0000

    postAPIerror

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const initializeDbAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3001, () => {
-      console.log("Server Running at http://localhost:3001");
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000");
     });
   } catch (error) {
     console.log(`DB Error: ${error.message}`);
@@ -27,9 +27,39 @@ const initializeDbAndServer = async () => {
 };
 initializeDbAndServer();
 
+let convertingStatesObject = (state) => {
+  return {
+    stateId: state.state_id,
+    stateName: state.state_name,
+    population: state.population,
+  };
+};
+
 app.get("/states/", async (request, response) => {
   const getStatesQuery = `SELECT * FROM state;`;
 
   const state = db.all(getStatesQuery);
   response.send(state);
+});
+
+app.get("/states/:stateId/", async (request, response) => {
+  const { stateId } = request.params;
+  const getStateBasedOnIdQuery = `
+    SELECT 
+    *
+    FROM
+    state
+    WHERE
+    state_id = ${stateId};`;
+  const state = db.all(getStateBasedOnIdQuery);
+  response.send(convertingStatesObject(state));
+});
+
+app.post("/districts/", async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const postDistrictQuery = `
+ INSERT INTO district (district_name,state_id,cases,cured,active,deaths)
+ VALUES('${districtName}',${stateId},${cases},${cured},${active},${deaths});`;
+  await db.run(postDistrictQuery);
+  response.send("District Successfully Added");
 });
